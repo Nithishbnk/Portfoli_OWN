c9ee93fde2934561bb1a0292f8aba78ea4869119
Ivsrk <ivsrk.itha@gmail.com>
2021-05-01 18:29:01 +0530

    dummy commit
    s

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React JS APP
+          Learn React JS APP by Nithish And Itha
         </a>
       </header>
     </div>
