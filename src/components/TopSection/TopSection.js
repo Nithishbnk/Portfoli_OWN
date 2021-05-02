f28700b0f9fd850360caf468a7e884d235afe252
Nithish kumar <bommireddynithishkumar@gmail.com>
2021-05-02 12:00:47 +0530

    Jumbotron added

--- src/components/TopSection/TopSection.js
+++ src/components/TopSection/TopSection.js
@@ -1,22 +1,13 @@
 import React, {Component} from 'react'
-import { SplitButton, MenuItem } from 'react-bootstrap';
+import  "./TopSection.css"
 class TopSection extends Component {
-
-  // state = { theme: null }
-  //
-  // chooseTheme = (theme, evt) => {
-  //   evt.preventDefault();
-  //   if (theme.toLowerCase() === 'reset') { theme = null }
-  //   this.setState({ theme });
-  // }
-
   render() {
 
     return (
       <div>
       <div class="jumbotron jumbotron-fluid">
         <div class="container">
-          <h1 class="display-4">Fluid jumbotron</h1>
+          <h1 class="display-4 name">Fluid jumbotron</h1>
           <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
         </div>
       </div>
